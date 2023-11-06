5e411e45fb3b04dfdb3a9f5e873afa592ca8a0d3
Dmitri Popov <operator@cv.dp-net.com>
2023-11-06 09:18:31 +0100

    Update TFTP server to Golang TFTP library V3

--- src/memtftpd/main.go
+++ src/memtftpd/main.go
@@ -2,63 +2,79 @@ package main
 
 import (
 	"fmt"
-	"github.com/pin/tftp"
-	"net"
+	"github.com/pin/tftp/v3"
 	"io"
 	"bytes"
-	"flag"
 	"os"
-	"log"
+	"time"
 )
 
+// Map stores "files" in memory.
 var m map[string][]byte
 
-func HandleWrite(filename string, r *io.PipeReader) {
+// Handler for write (aka PUT) requests.
+func writeHandler(filename string, wt io.WriterTo) error {
 	_, exists := m[filename]
 	if exists {
-		r.CloseWithError(fmt.Errorf("File already exists: %s", filename))
-		return
+		return fmt.Errorf("File already exists: %s", filename)
 	}
 	buffer := &bytes.Buffer{}
-	c, e := buffer.ReadFrom(r)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "Can't receive %s: %v\n", filename, e)
-	} else {
-		fmt.Fprintf(os.Stderr, "Received %s (%d bytes)\n", filename, c)
-		m[filename] = buffer.Bytes()
+	_,err := wt.WriteTo(buffer)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't receive %s: %v\n", filename, err)
+		return err
 	}
+	m[filename] = buffer.Bytes()
+	return nil
 }
 
-func HandleRead(filename string, w *io.PipeWriter) {
+// Hander for read (aka GET) requests.
+func readHandler(filename string, rt io.ReaderFrom) error {
 	b, exists := m[filename]
-	if exists {
-		buffer := bytes.NewBuffer(b)
-		c, e := buffer.WriteTo(w)
-		if e != nil {
-			fmt.Fprintf(os.Stderr, "Can't send %s: %v\n", filename, e)
-		} else {
-			fmt.Fprintf(os.Stderr, "Sent %s (%d bytes)\n", filename, c)
-		}
-		w.Close()
-	} else {
-		w.CloseWithError(fmt.Errorf("File not exists: %s", filename))
+	if !exists {
+		return fmt.Errorf("File not found: %s", filename)
 	}
+	buffer := bytes.NewBuffer(b)
+	_, err := rt.ReadFrom(buffer)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't send %s: %v\n", filename, err)
+	}
+	return nil
+}
+
+
+// Hook for logging on every transfer completion or failure.
+type logHook struct {}
+func (h *logHook) OnSuccess(stats tftp.TransferStats) {
+	fmt.Printf("Transfer of %s to %s complete\n", stats.Filename, stats.RemoteAddr)
+}
+func (h *logHook) OnFailure(stats tftp.TransferStats, err error) {
+	fmt.Printf("Transfer of %s to %s failed: %v\n", stats.Filename, stats.RemoteAddr, err)
 }
 
 func main() {
 	m = make(map[string][]byte)
-	addrStr := flag.String("l", ":69", "Address to listen")
-	flag.Parse()
-	addr, e := net.ResolveUDPAddr("udp", *addrStr)
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
-		return
+
+	// Start the server.
+	s := tftp.NewServer(readHandler, writeHandler)
+	s.SetHook(&logHook{})
+	go func() {
+		err := s.ListenAndServe(":69")
+		if err != nil {
+			fmt.Fprintf(os.Stdout, "Can't start server: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+
+	// Do some other stuff.
+	for {
+		_, ok := m["secret-magic-shutdown-file"]
+		if ok {
+			break
+		}
+		time.Sleep(5 * time.Minute)
 	}
-	log := log.New(os.Stderr, "", log.Ldate | log.Ltime)
-	s := tftp.Server{addr, HandleWrite, HandleRead, log}
-	e = s.Serve()
-	if e != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", e)
-		os.Exit(1);
-	}
+
+	// Eventually shutdown the server.
+	s.Shutdown()
 }
